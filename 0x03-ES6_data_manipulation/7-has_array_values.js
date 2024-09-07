2cac7575b15f02116f07f3b043a20ae9c39d6b9d
trust <isaactrust447@gmail.com>
2024-09-07 21:49:57 +0100

    go

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,4 +1,11 @@
-export default function setFromArray(list) {
-    return new Set(list);
-  }
+/* eslint-disable array-callback-return */
+export default function hasValuesFromArray(set, list) {
+    let verdict = true;
+    list.map((x) => {
+      if (!set.has(x)) {
+        verdict = false;
+      }
+    });
   
+    return verdict;
+  }
