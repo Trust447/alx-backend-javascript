c969e8bb0ee731938d3ad7429c17b503e74b99a0
trust <isaactrust447@gmail.com>
2024-09-07 21:25:43 +0100

    go

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,21 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-prototype-builtins */
 
-export default function updateStudentGradeByCity(list, city, newGrades) {
+export default function updateStudentGradeByCity(list, city, newGrade) {
     return list
-      .filter((student) => student.location === city)
+      .filter((obj) => obj.location === city)
       .map((student) => {
-        const gradeEntry = newGrades.find((entry) => entry.studentId === student.id);
-        student.grade = gradeEntry ? gradeEntry.grade : 'N/A';
+        newGrade.forEach((studentGrade) => {
+          if (studentGrade.studentId === student.id) {
+            // eslint-disable-next-line no-param-reassign
+            student.grade = studentGrade.grade;
+          }
+        });
+  
+        if (!student.hasOwnProperty('grade')) {
+          student.grade = 'N/A';
+        }
+  
         return student;
       });
   }
