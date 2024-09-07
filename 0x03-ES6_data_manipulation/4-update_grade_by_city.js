64acf53f12510826c2476f84e1e1425177bd199a
trust <isaactrust447@gmail.com>
2024-09-07 21:21:29 +0100

    go

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,22 +1,12 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-prototype-builtins */
-export default function updateStudentGradeByCity(list, city, newGrade) {
+
+export default function updateStudentGradeByCity(list, city, newGrades) {
     return list
-      .filter((obj) => obj.location === city)
+      .filter((student) => student.location === city)
       .map((student) => {
-      //   console.log(newGrade);
-        newGrade.map((studentGrade) => {
-          if (studentGrade.studentId === student.id) {
-            // eslint-disable-next-line no-param-reassign
-            student.grade = studentGrade.grade;
-          }
-  
-          if (!student.hasOwnProperty('grade')) {
-            student.grade = 'N/A';
-          }
-          return student;
-        });
-  
+        const gradeEntry = newGrades.find((entry) => entry.studentId === student.id);
+        student.grade = gradeEntry ? gradeEntry.grade : 'N/A';
         return student;
       });
   }
