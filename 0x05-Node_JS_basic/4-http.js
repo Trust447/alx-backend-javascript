263eeb2b3b38097dbb519d06a1253324ee7e0c70
trust <isaactrust447@gmail.com>
2024-10-02 13:59:56 +0100

    good

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,25 +1,14 @@
 const http = require('http');
 
-const PORT = 1245;
-const HOST = 'localhost';
-const app = http.createServer();
+const host = '127.0.0.1';
+const port = 1245;
 
-app.on('request', (_, res) => {
-  const responseText = 'Hello Holberton School!';
-
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', responseText.length);
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
-  
-  // Write the response
-  res.write(Buffer.from(responseText));
-
-  // End the response
-  res.end();
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
 });
 
-app.listen(PORT, HOST, () => {
-  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
-});
+app.listen(port, host, () => {});
 
-// Ensure there's a newline at the end of the file for linting
+module.exports = app;
